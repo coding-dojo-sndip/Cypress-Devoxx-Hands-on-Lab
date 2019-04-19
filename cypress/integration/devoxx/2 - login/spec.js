04f263b031df40102dd0b8f180c46faa0f5a348c
Romain Warnan <romain.warnan@gmail.com>
2019-04-19 12:20:14 +0200

    Début du TP2, formulaire de login

--- cypress/integration/devoxx/2 - login/spec.js
+++ cypress/integration/devoxx/2 - login/spec.js
@@ -2,8 +2,12 @@ describe('Login page', function() {
   it('should display nicely', function() {
     cy.visit('/login')
     // TODO check the 'Sign in' label is visibke
+    cy.contains('Sign in').should('exist')
 
     // TODO check the form is nicely displayed (email, password, submit button)
+    cy.get('input[type=email]').should('be.visible').should('have.attr', 'placeholder', 'Email')
+    cy.get('input[type=password]').should('be.visible').should('have.attr', 'placeholder', 'Password')
+    cy.get('.btn[type=submit]').should('be.visible').should('have.text', 'Sign in')
   })
 
   it('should display error message when empty email and password are submitted', function() {
@@ -31,3 +35,23 @@ describe('Login page', function() {
     // TODO happy path : use cypress@devoxx.fr/cypressdevoxx to fill the form and ensure you are logged
   })
 })
+
+describe('hooks', function() {
+  before(function() {
+    // runs before all tests in this block
+  });
+
+  after(function() {
+    // runs after all tests in this block
+  });
+
+  beforeEach(function() {
+    // runs before each test in this block
+  });
+
+  afterEach(function() {
+    // runs after each test in this block
+  });
+
+  // test cases
+});
